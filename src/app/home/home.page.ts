64306949893a0b83cb296bb28e08a0904d1d1c18
Cesar Josue Vargas Sosa <34899519+josuevargassosa@users.noreply.github.com>
2026-01-21 00:54:31 -0500

    Refactor home page layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonLabel,
+  IonSearchbar,
+  IonSegment,
+  IonSegmentButton,
+  IonTabBar,
+  IonTabButton,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonFooter,
+    IonHeader,
+    IonLabel,
+    IonSearchbar,
+    IonSegment,
+    IonSegmentButton,
+    IonTabBar,
+    IonTabButton,
+    IonToolbar,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  currentSection = 'articles';
 }
