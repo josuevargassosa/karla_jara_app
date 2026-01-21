b89a33532398f18fe8ea5df29cefbea0aa3f5d2f
Cesar Josue Vargas Sosa <34899519+josuevargassosa@users.noreply.github.com>
2026-01-21 00:38:59 -0500

    Implement login screen with reactive form

--- src/app/core/services/mock-auth.service.ts
+++ src/app/core/services/mock-auth.service.ts
@@ -6,6 +6,20 @@ import { Usuario } from '../../models/app.models';
   providedIn: 'root',
 })
 export class MockAuthService {
+  login(): void {
+    localStorage.setItem('auth_token', 'fake-token');
+    localStorage.setItem('user_role', 'admin');
+    localStorage.setItem('user_name', 'KARLA');
+    localStorage.removeItem('guest');
+  }
+
+  enableGuestMode(): void {
+    localStorage.setItem('guest', 'true');
+    localStorage.setItem('user_role', 'guest');
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('user_name');
+  }
+
   getCurrentUser(): Usuario | null {
     const token = localStorage.getItem('auth_token');
     const role = localStorage.getItem('user_role');
